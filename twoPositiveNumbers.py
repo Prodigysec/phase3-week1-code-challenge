99500a64d4bb13c83430a05ec565b118dba86fc1
Ian Raburu <williamraburu@gmail.com>
2023-08-20 23:54:11 +0300

    Return True if exactly 2 of the 3 intergers are positive numbers, and False otherwise

--- twoPositiveNumbers.py
+++ twoPositiveNumbers.py
@@ -1,2 +1,11 @@
 def two_positives(a, b, c):
-    pass
+    positive_count = 0
+
+    if a > 0:
+        positive_count += 1
+    if b > 0:
+        positive_count += 1
+    if c > 0:
+        positive_count += 1
+
+    return positive_count == 2
