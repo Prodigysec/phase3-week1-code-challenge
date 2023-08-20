d37b28825b0be4d314995087c8dc4dbcd7aad337
Ian Raburu <williamraburu@gmail.com>
2023-08-20 23:54:43 +0300

    Add test cases

--- twoPositiveNumbers.py
+++ twoPositiveNumbers.py
@@ -9,3 +9,10 @@ def two_positives(a, b, c):
         positive_count += 1
 
     return positive_count == 2
+
+print(two_positives(2, 4, -3))  # Output: True
+print(two_positives(-4, 6, 8))  # Output: True
+print(two_positives(4, -6, 9))  # Output: True
+print(two_positives(-4, 6, 0))  # Output: False
+print(two_positives(4, 6, 10))  # Output: False
+print(two_positives(-14, -3, -4))  # Output: False
