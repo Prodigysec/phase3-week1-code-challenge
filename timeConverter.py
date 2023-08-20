87591347ac670f195972d8098e1c74334e4c21d7
Ian Raburu <williamraburu@gmail.com>
2023-08-20 22:17:06 +0300

    Check if the arguments are valid

--- timeConverter.py
+++ timeConverter.py
@@ -1,3 +1,11 @@
 def convert_to_24_hour(hour, minute, period):
-    pass
+    if (hour > 0 and hour <= 12) and (minute > 0 and minute <= 59) and (period == "am" or period == "pm"):
+        if hour == 12:
+            if period == "am":
+                hour = 0
+        elif hour != 12:
+            if period == "pm":
+                hour += 12
+    else:
+        return "Not a valid time format"
     
