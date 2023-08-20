fdf76c67fe953f7979cbe8caea0b56c7c1e57926
Ian Raburu <williamraburu@gmail.com>
2023-08-21 01:50:32 +0300

    Get the highest value and return it

--- consonantValue.py
+++ consonantValue.py
@@ -1,2 +1,14 @@
 def solve(lowercaseString):
-    pass
+    vowels = 'aeiou'
+    max_value = 0
+    current_value = 0
+
+    for char in lowercaseString:
+        if char not in vowels:
+            current_value += ord(char) - ord('a') + 1
+            max_value = max(max_value, current_value)
+        else:
+            current_value = 0
+
+    return max_value
+
