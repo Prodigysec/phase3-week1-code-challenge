af0c7ea1f236c5c1be434c3b985752ce174a6d12
Ian Raburu <williamraburu@gmail.com>
2023-08-20 22:19:03 +0300

    Add tests

--- timeConverter.py
+++ timeConverter.py
@@ -13,3 +13,9 @@ def convert_to_24_hour(hour, minute, period):
     time_24_hour = f"{hour:02d}{minute:02d}"
     return time_24_hour
 
+
+print(convert_to_24_hour(0, 30, "am"))  # Output: "Not a valid number"
+print(convert_to_24_hour(13, 15, "pm"))  # Output: "Not a valid number"
+print(convert_to_24_hour(1, 37, "pm"))  # Output: "1337"
+print(convert_to_24_hour(12, 15, "am"))  # Output: "0015"
+print(convert_to_24_hour(12, 18, "pm"))  # Output: "1218"
